9b819a6e373780431fd587d5965c095b49d21c5b
Ben Wanicur <bwanicur@gmail.com>
2026-09-25 13:50:58 -0700

    more efficient rank lookup

--- deck.rb
+++ deck.rb
@@ -1,33 +1,31 @@
 class Deck
   attr_reader :cards
 
-  ORDERED_CARD_SET = %w[
-    2
-    3
-    4
-    5
-    6
-    7
-    8
-    9
-    10
-    Jack
-    Queen
-    King
-    Ace
-  ].freeze
+  ORDERED_CARD_SET_RANK = {
+    2 => 0,
+    3 => 1,
+    4 => 2,
+    5 => 3,
+    6 => 4,
+    7 => 5,
+    8 => 6,
+    9 => 7,
+    10 => 8,
+    "Jack" => 9,
+    "Queen" => 10,
+    "King" => 11,
+    "Ace" => 12
+  }.freeze
 
   NUM_SUITS = 4
 
   def self.rank(card)
-    return -1 if card.nil?
-
-    ORDERED_CARD_SET.index(card)
+    ORDERED_CARD_SET_RANK[card]
   end
 
   def initialize
     @cards = []
-    ORDERED_CARD_SET.each do |card|
+    ORDERED_CARD_SET_RANK.keys.each do |card|
       NUM_SUITS.times { @cards << card }
     end
   end
